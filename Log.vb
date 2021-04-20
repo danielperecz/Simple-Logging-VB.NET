0547e896091f1a35311a31e2d3c37375243d76d6
Daniel <42898223+danielperecz@users.noreply.github.com>
2021-04-20 18:44:06 +0100

    Refactoring

--- Log.vb
+++ Log.vb
@@ -1,46 +1,22 @@
-' Simple but dynamic class for creating log files. Example line from a log file:
-' 2020-04-29 13:13:24.495 | 1  | admin | btnSubmitDelete_Click | Deleted hours worked. | Original Range: 2020-04-24 09:00 to 2020-04-24 17:00
-
 Public Class Log
 
     ' Log file settings.
-    Private ReadOnly LINE_LIMIT As Integer = 10000          ' Max line limit for file. If file reaches this number, a new file will be created for next log.
-    Private ReadOnly EMPLOYEE_ID_PADDING As Integer = 5
-    Private ReadOnly ROLE_PADDING As Integer = 5
+    Private ReadOnly LINE_LIMIT As Integer = 10000          ' Max line limit for file. If file reaches this number, a new file will be created.
+    Private ReadOnly LOG_TYPE_PADDING As Integer = 6
     Private ReadOnly METHOD_PADDING As Integer = 40
-    Private ReadOnly DESCRIPTION_PADDING As Integer = 60
+    Private ReadOnly RESULT_PADDING As Integer = 5
 
-    ' Log file data.
-    Private CurrentDateTime As String
-    Private Employee_ID As String
-    Private Role As String
-    Private Method As String
-    Private Description As String
-    Private Payload As String
-
-    Public Sub New(CurrentDateTime As Date, Employee_ID As String, Role As String, Method As String, Description As String, Optional Payload As String = "None")
-
-        Me.CurrentDateTime = CurrentDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff")
-        Me.Employee_ID = Employee_ID
-        Me.Role = Role
-        Me.Method = Method
-        Me.Description = Description
-        Me.Payload = Payload
-        
-        WriteToFile()
-
-    End Sub
-
-    Private Sub WriteToFile()
+    Public Sub WriteToFile(Method As String, LogType As String, Optional Result As String = "None", Optional Payload As String = "None")
         ' Write new line to a text file without deleting any existing data the file may contain.
 
         Dim file As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(file:=SelectFile(), append:=True)
-        file.WriteLine(CurrentDateTime & " | " &
-                       Employee_ID.PadRight(EMPLOYEE_ID_PADDING, " ") & " | " &
-                       Role.PadRight(ROLE_PADDING, " ") & " | " &
-                       Method.PadRight(METHOD_PADDING, " ") & " | " &
-                       Description.PadRight(DESCRIPTION_PADDING, " ") & " | " &
-                       Payload)
+        file.WriteLine(
+            Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " | " +
+            LogType.ToUpper().PadRight(LOG_TYPE_PADDING, " ") + " | " +
+            Method.PadRight(METHOD_PADDING, " ") + " | " +
+            Result.PadRight(RESULT_PADDING, " ") + " | " +
+            Payload
+        )
         file.Close()
 
     End Sub
@@ -48,9 +24,8 @@ Public Class Log
     Private Function SelectFile() As String
         ' Return full path to log file to be used.
         
-        ' LogsPath is a path to your log folder. E.g.: "C:\Application\Logs".
-        Dim newFilePath As String = LogsPath & "\" & DateTime.Now.ToString("yyyy.MM.dd_HH.mm.ss") & ".txt"
-        Dim folder As IO.DirectoryInfo = New IO.DirectoryInfo(LogsPath)
+        Dim newFilePath As String = GlobalVariables.Logs & "\" & Date.Now.ToString("yyyy.MM.dd_HH.mm.ss") & ".txt"
+        Dim folder As IO.DirectoryInfo = New IO.DirectoryInfo(GlobalVariables.Logs)
 
         If folder.GetFiles.Length = 0 Then
             ' If Logs folder is empty.
